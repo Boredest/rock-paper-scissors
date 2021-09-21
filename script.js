9eb9262cf40202e305ff3786c6c384464af3bbaf
Shinu <hodge195@gmail.com>
2021-09-21 09:56:56 -0700

    Fixed up UI. Need to finish making it functional

--- script.js
+++ script.js
@@ -1,93 +1,122 @@
-let playerScore = 0;
+
+let playerScore = 0
 let computerScore = 0;
 let roundsPlayed = 0;
 let gameLimit = 5;
 
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById("paper");
+const scissorsButton = document.getElementById("scissors");
 
-function game(){
-    //loop for 5 rounds
+const totalScore = document.getElementById("Score");
+let playerScore = document.getElementById("playerScore");
+totalScore.textContent = playerScore + " - " + computerScore;
 
-    while(roundsPlayed < gameLimit){
-        const computerSelection = computerPlay();
-        const playerSelection = playerChoice();
-        
-       playRound(playerSelection,computerSelection);
-        //console.log(computerScore);
-        console.log(roundsPlayed);
-       
-    }
-   
+
+//const computerSelection = computerPlay();
+let playerSelection = "";
+const computerSelection = computerPlay();
+
+function playerChoice() {
+    paperButton.addEventListener("click", function () {
+        playerSelection = "paper";
+        console.log(playerSelection);
+        playRound(playerSelection, computerSelection);
+
+    });
+
+    rockButton.addEventListener("click", function () {
+        playerSelection = "rock";
+        console.log(playerSelection);
+        playRound(playerSelection, computerSelection);
+
+    });
+
+    scissorsButton.addEventListener("click", function () {
+        playerSelection = "scissors";
+        console.log(playerSelection);
+        playRound(playerSelection, computerSelection);
+
+    });
 }
 
-function computerPlay(){
-    const computerChoice = ["Rock","Paper","Scissors"];
-    let randomChoice = Math.floor(Math.random()*computerChoice.length);
 
-    if(randomChoice === 0){
+
+function game() {
+    playerChoice();
+
+}
+
+
+function computerPlay() {
+    const computerChoice = ["Rock", "Paper", "Scissors"];
+    let randomChoice = Math.floor(Math.random() * computerChoice.length);
+
+    if (randomChoice === 0) {
         return "Rock";
     }
-    else if(randomChoice == 1){
+    else if (randomChoice == 1) {
         return "Paper";
-    } 
+    }
 
-    else if(randomChoice == 2){
+    else if (randomChoice == 2) {
         return "Scissors";
     }
 }
 
-function playerChoice() {
-    return prompt("Rock, Paper, or Scissors").toLowerCase();
-}
 
 
 
-function playRound(playerSelection, computerSelection){
+function playRound(playerSelection, computerSelection) {
     //play a round
 
- if(playerSelection === computerSelection.toLowerCase()){
-     roundsPlayed++;
+    if (playerSelection === computerSelection.toLowerCase()) {
+        roundsPlayed++;
         return "It's a tie!";
     }
 
- else if(playerSelection === "rock" && computerSelection === "Paper"){
-    roundsPlayed++;
-    computerScore++;
-     return "YOu Lose";
- }
- else if(playerSelection === "paper" && computerSelection === "Scissors"){
-    roundsPlayed++;
-     return "YOu Lose";
- }
+    else if (playerSelection === "rock" && computerSelection === "Paper") {
+        roundsPlayed++;
+        computerScore++;
+        return "YOu Lose";
+    }
+    else if (playerSelection === "paper" && computerSelection === "Scissors") {
+        roundsPlayed++;
+        return "YOu Lose";
+    }
 
- else if(playerSelection === "scissors" && computerSelection === "Rock"){
-    roundsPlayed++;
-    return "YOu Lose";
+    else if (playerSelection === "scissors" && computerSelection === "Rock") {
+        roundsPlayed++;
+        return "YOu Lose";
+    }
+
+    else if (computerSelection === "Rock" && playerSelection == "paper") {
+        roundsPlayed++;
+        playerScore++;
+        return "You Win!"
+    }
+
+    else if (computerSelection === "Paper" && playerSelection === "scissors") {
+        roundsPlayed++;
+        playerScore++;
+        return "You Win!"
+    }
+
+    else if (computerSelection === "Scissors" && playerSelection === "rock") {
+        roundsPlayed++;
+        playerScore++;
+        return "You Win!"
+
+    }
+
 }
 
-else if(computerSelection === "Rock" && playerSelection == "paper"){
-    roundsPlayed++;
-    return "You Win!"
-}
 
-else if(computerSelection === "Paper" && playerSelection === "scissors"){
-    roundsPlayed++;
-    return "You Win!"
-}
 
-else if(computerSelection === "Scissors" && playerSelection === "rock"){
-    roundsPlayed++;
-    return "You Win!"
-    
-}
 
-}
+
 
 game();
-
-
-
-
-
 
 
 
