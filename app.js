0eeb84e20dc131a5781196aaf99b91bf340c53fa
Boredest <19357608+Boredest@users.noreply.github.com>
2025-10-04 23:38:59 +0900

    Added js file and function for computer choice.

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("test");
+let choices = ["rock", "paper", "scissors"]
+
+function getComputerChoice(){
+    let computerChoice = Math.floor(Math.random() * choices.length);
+    console.log(computerChoice);
+
+    if (computerChoice == 0){
+        console.log("rock");
+        return "rock";
+    }
+
+    else if (computerChoice == 1){
+        console.log("paper");
+        return "paper";
+    }
+
+    else {
+        console.log("scissors");
+        return "scissors";
+    }
+
+}
+
+getComputerChoice();
