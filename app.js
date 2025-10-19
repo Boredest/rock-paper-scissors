34daacc98672e85e56b051bb45c922a2823fccdf
Boredest <19357608+Boredest@users.noreply.github.com>
2025-10-20 00:09:02 +0900

    Rough draft of UI done. Buttons working, but the logic needs to be updated so the game can end.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 let choices = ["rock", "paper", "scissors"];
 
-let roundLimit = 5;
+const roundLimit = 5;
+
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
   let computerChoice = Math.floor(Math.random() * choices.length);
@@ -14,42 +17,24 @@ function getComputerChoice() {
   }
 }
 
-function getHumanChoice() {
-  //let humanChoice = prompt("Please enter your choice.").toLowerCase();
-
-  return humanChoice;
-}
-
-function playGame() {
-  let humanScore = 0;
-  let computerScore = 0;
-
-  for (let i = 0; i < roundLimit; i++) {
-    let humanChoice = getHumanChoice();
-    let computerChoice = getComputerChoice();
-    playRound(humanChoice, computerChoice);
+function playRound(humanChoice, computerChoice) {
+  console.log(humanChoice, computerChoice);
+  if (humanChoice == computerChoice) {
+  } else if (humanChoice == "rock" && computerChoice == "scissors") {
+    humanScore++;
+  } else if (humanChoice == "paper" && computerChoice == "rock") {
+    humanScore++;
+  } else if (humanChoice == "scissors" && computerChoice == "paper") {
+    humanScore++;
+  } else if (humanChoice == "rock" && computerChoice == "paper") {
+    computerScore++;
+  } else if (humanChoice == "paper" && computerChoice == "scissors") {
+    computerScore++;
+  } else if (humanChoice == "scissors" && computerChoice == "rock") {
+    computerScore++;
   }
-
-  function playRound(humanChoice, computerChoice) {
-    console.log("Human " + humanChoice, computerChoice);
-    if (humanChoice == computerChoice) {
-    } else if (humanChoice == "rock" && computerChoice == "scissors") {
-      humanScore++;
-    } else if (humanChoice == "paper" && computerChoice == "rock") {
-      humanScore++;
-    } else if (humanChoice == "scissors" && computerChoice == "paper") {
-      humanScore++;
-    } else if (humanChoice == "rock" && computerChoice == "paper") {
-      computerScore++;
-    } else if (humanChoice == "paper" && computerChoice == "scissors") {
-      computerScore++;
-    } else if (humanChoice == "scissors" && computerChoice == "rock") {
-      computerScore++;
-    }
-    console.log(humanScore, computerScore);
-  }
-
-  calculateScore(humanScore, computerScore);
+  console.log(humanScore, computerScore);
+  // calculateScore(humanScore, computerScore);
 }
 
 function calculateScore(humanScore, computerScore) {
@@ -59,22 +44,27 @@ function calculateScore(humanScore, computerScore) {
     console.log(humanScore > computerScore ? "Human Wins!" : "Comp wins.");
   }
 }
-let buttons = document.querySelector(".buttons");
+const buttonsContainer = document.querySelector(".buttons");
 
-buttons.addEventListener("click", (event) => {
-  let target = event.target;
+buttonsContainer.addEventListener("click", (event) => {
+  const clickedButton = event.target.closest("button");
+  if (!clickedButton) return;
+  const playerChoice = clickedButton.id.toLowerCase();
 
-  switch (target.id) {
-    case "Rock":
-      console.log("Human selected rock");
+  switch (playerChoice) {
+    case "rock":
+      console.log(playerChoice);
+      playRound(playerChoice, getComputerChoice());
       break;
 
-    case "Paper":
-      console.log("Human selected paper");
+    case "paper":
+      console.log(playerChoice);
+      playRound(playerChoice, getComputerChoice());
       break;
 
-    case "Scissors":
-      console.log("Human selected scissors");
+    case "scissors":
+      console.log(playerChoice);
+      playRound(playerChoice, getComputerChoice());
       break;
   }
 });
